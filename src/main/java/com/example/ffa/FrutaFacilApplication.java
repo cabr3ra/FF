1aec48397c060a0a85f2a2d833226ede28ad80a0
Uri <uri.cabrera2000@gmail.com>
2024-05-08 17:28:26 +0200

    Cambios

--- src/main/java/com/example/ffa/FrutaFacilApplication.java
+++ src/main/java/com/example/ffa/FrutaFacilApplication.java
@@ -3,9 +3,6 @@ package com.example.ffa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-
-import model.TUser;
 
 @SpringBootApplication
 @EntityScan(basePackages = "model")
@@ -15,21 +12,5 @@ public class FrutaFacilApplication {
 		SpringApplication.run(FrutaFacilApplication.class, args);
 		
 	}
-	
-	@Bean
-    public void initData() {
-        // Insertar usuarios hardcodeados
-        TUser user1 = new TUser();
-        user1.setNameUser("Nombre1");
-        user1.setEmailUser("correo1@example.com");
-        // Setear otros atributos según lo necesites
-
-        TUser user2 = new TUser();
-        user2.setNameUser("Nombre2");
-        user2.setEmailUser("correo2@example.com");
-        // Setear otros atributos según lo necesites
-
-        // Agrega más inserciones según sea necesario
-    }
 
 }
