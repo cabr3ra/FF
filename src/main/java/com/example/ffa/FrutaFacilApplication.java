b77dc39e68a091230891330f3d14f2f21fcd3978
TuNombre <tu@email.com>
2024-05-09 15:33:18 +0200

    cambios

--- src/main/java/com/example/ffa/FrutaFacilApplication.java
+++ src/main/java/com/example/ffa/FrutaFacilApplication.java
@@ -1,14 +1,8 @@
 package com.example.ffa;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-
-import model.TUser;
 
 @SpringBootApplication
 @EntityScan(basePackages = "model")
@@ -18,27 +12,4 @@ public class FrutaFacilApplication {
 		SpringApplication.run(FrutaFacilApplication.class, args);
 		
 	}
-	
-	@Bean
-	public List<TUser> initData() {
-	    List<TUser> users = new ArrayList<>();
-
-	    // Insertar usuarios hardcodeados
-	    TUser user1 = new TUser();
-	    user1.setNameUser("Nombre1");
-	    user1.setEmailUser("correo1@example.com");
-	    // Setear otros atributos según lo necesites
-	    users.add(user1);
-
-	    TUser user2 = new TUser();
-	    user2.setNameUser("Nombre2");
-	    user2.setEmailUser("correo2@example.com");
-	    // Setear otros atributos según lo necesites
-	    users.add(user2);
-
-	    // Agrega más inserciones según sea necesario
-
-	    return users;
-	}
-
 }
